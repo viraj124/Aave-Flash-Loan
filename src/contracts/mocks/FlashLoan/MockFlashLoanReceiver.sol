d0bded8eb0cf8e0fe6156bb3f533f063efe4d4b9
Viraz <virajm72@gmail.com>
2019-11-11 22:55:41 +0530

    Changed the uniswap address now, it isn't same from earlier

--- src/contracts/mocks/FlashLoan/MockFlashLoanReceiver.sol
+++ src/contracts/mocks/FlashLoan/MockFlashLoanReceiver.sol
@@ -42,7 +42,7 @@ contract MockFlashLoanReceiver is FlashLoanReceiverBase {
         //BOT LOGIC
         token.approve(0xB4ca10f43caF503b7Aa0a77757B99c78212D6b92, _amount);
         // Exchange for token -> eth
-        UniswapExchange followerUniSwapExchange = UniswapExchange(0xB4ca10f43caF503b7Aa0a77757B99c78212D6b92);
+        UniswapExchange followerUniSwapExchange = UniswapExchange(0xD3E51Ef092B2845f10401a0159B2B96e8B6c3D30);
 
         uint256 DEADLINE = block.timestamp + 200;
         // Swap token -> Eth
